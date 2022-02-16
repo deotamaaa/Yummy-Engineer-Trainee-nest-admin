40f980bc8fdc5a4bfe44941066af41a2a988d166
Deotama <deotama11@gmail.com>
2022-02-16 10:18:50 +0700

    Video 20 Done. Add pagination.

--- dist/user/user.service.d.ts
+++ dist/user/user.service.d.ts
@@ -4,6 +4,7 @@ export declare class UserService {
     private readonly userRepository;
     constructor(userRepository: Repository<User>);
     all(): Promise<User[]>;
+    paginate(page?: number): Promise<any>;
     create(data: any): Promise<User>;
     findOne(condition: any): Promise<User>;
     update(id: number, data: any): Promise<any>;
