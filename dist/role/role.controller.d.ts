7985970fa8c88762b6716f0a409a6e6a192af341
Deotama <deotama11@gmail.com>
2022-02-17 19:14:25 +0700

    Video 24 Done. Add role permission many to many.

--- dist/role/role.controller.d.ts
+++ dist/role/role.controller.d.ts
@@ -3,8 +3,8 @@ export declare class RoleController {
     private roleService;
     constructor(roleService: RoleService);
     all(): Promise<import("./role.entity").Role[]>;
-    create(name: String): Promise<import("./role.entity").Role>;
+    create(name: String, ids: number[]): Promise<import("./role.entity").Role>;
     get(id: number): Promise<import("./role.entity").Role>;
-    update(id: number, name: String): Promise<import("./role.entity").Role>;
+    update(id: number, name: String, ids: number[]): Promise<import("./role.entity").Role>;
     delete(id: number): Promise<any>;
 }
