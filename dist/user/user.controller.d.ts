bf9ed632b1cac09fd86d3c64b86a230dcbab36bb
Deotama <deotama11@gmail.com>
2022-02-18 18:04:12 +0700

    Video 26 Done. Added Circular Dependecies.

--- dist/user/user.controller.d.ts
+++ dist/user/user.controller.d.ts
@@ -2,12 +2,17 @@ import { UserService } from './user.service';
 import { User } from './models/user.entity';
 import { UserCreateDto } from './models/user-create.dto';
 import { UserUpdateDto } from './models/user-update.dto';
+import { AuthService } from 'src/auth/auth.service';
+import { Request } from 'express';
 export declare class UserController {
     private userService;
-    constructor(userService: UserService);
+    private authService;
+    constructor(userService: UserService, authService: AuthService);
     all(page?: number): Promise<import("../common/paginated-result.interface").PaginatedResult>;
     create(body: UserCreateDto): Promise<User>;
     get(id: number): Promise<any>;
+    updateInfo(request: Request, body: UserUpdateDto): Promise<any>;
+    updatePassword(request: Request, password: string, password_confirm: string): Promise<any>;
     update(id: number, body: UserUpdateDto): Promise<any>;
     delete(id: number): Promise<any>;
 }
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthService {
+
+    constructor(private jwtService: JwtService) {
+
+    }
+
+    async userId(request: Request): Promise<number> {
+        const cookie = request['jwt'];
+
+        // Get data from the Cookie
+        const data = await this.jwtService.verifyAsync(cookie);
+
+        // Get user from the database
+        return data['id'];
+    }
+}
