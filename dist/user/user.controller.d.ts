63405245ea184dd0b49965e713f76fa08c060a14
Deotama <deotama11@gmail.com>
2022-02-18 10:44:07 +0700

    Video 25 Done. Made Abstract service.

--- dist/user/user.controller.d.ts
+++ dist/user/user.controller.d.ts
@@ -5,9 +5,9 @@ import { UserUpdateDto } from './models/user-update.dto';
 export declare class UserController {
     private userService;
     constructor(userService: UserService);
-    all(page?: number): Promise<User[]>;
+    all(page?: number): Promise<import("../common/paginated-result.interface").PaginatedResult>;
     create(body: UserCreateDto): Promise<User>;
-    get(id: number): Promise<User>;
-    update(id: number, body: UserUpdateDto): Promise<User>;
+    get(id: number): Promise<any>;
+    update(id: number, body: UserUpdateDto): Promise<any>;
     delete(id: number): Promise<any>;
 }
