65b4f5a1b55d0f9a7df82a622bfcba05ef4d8000
Deotama <deotama11@gmail.com>
2022-02-21 10:11:40 +0700

    Video 31 Done. Added Export CSV Function.

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,5 +1,9 @@
-import { ClassSerializerInterceptor, Controller, Get, Query, UseGuards, UseInterceptors } from '@nestjs/common';
+import { ClassSerializerInterceptor, Controller, Get, Post, Query, Res, UseGuards, UseInterceptors } from '@nestjs/common';
+import { Response } from 'express';
+import { Parser } from 'json2csv';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { OrderItem } from './order-item.entity';
+import { Order } from './order.entity';
 import { OrderService } from './order.service';
 
 @UseInterceptors(ClassSerializerInterceptor)
@@ -14,5 +18,43 @@ export class OrderController {
         @Query('page') page: 1,
     ) {
         return this.orderService.paginate(page, ['order_items']);
+    }
+
+    @Post('export')
+    async export(@Res() res: Response) {
+        const parser = new Parser({
+            fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']
+        });
+
+        const orders = await this.orderService.all(['order_items']);
+
+        const json = [];
+
+        orders.forEach((o: Order) => {
+            json.push({
+                ID: o.id,
+                Name: o.name,
+                Email: o.email,
+                'Product Title': '',
+                Price: '',
+                Quantity: '',
+            });
+
+            o.order_items.forEach((i: OrderItem) => {
+                json.push({
+                    ID: '',
+                    Name: '',
+                    Email: '',
+                    'Product Title': i.product_title,
+                    Price: i.price,
+                    Quantity: i.quantity,
+                });
+            })
+        });
+
+        const csv = parser.parse(json);
+        res.header('Content-Type', 'text/csv');
+        res.attachment('orders.csv');
+        return res.send(csv);
     }
 }
